cef236a9934478d7b23cbcadef5ad20a51eb37e9
VitaliyZhark <vitaliy.zhark@gmail.com>
2023-02-15 14:55:11 +0000

    Update code
    Update code

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ var searchHistoryEl = $('#search-history');
 var currentDay = moment().format('M/DD/YYYY');
 const weatherIconUrl = 'http://openweathermap.org/img/wn/';
 var searchHistoryArray = loadSearchHistory();
+
+// END GLOBAL VARIABLES //
+
+// Define function to capitalize the first letter of a string
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+}
+
+//load cities from local storage and recreate history buttons
+function loadSearchHistory() {
+    var searchHistoryArray = JSON.parse(localStorage.getItem('search history'));
+
+    // if nothing in localStorage, create a new object to track all user info
+    if (!searchHistoryArray) {
+        searchHistoryArray = {
+            searchedCity: [],
+        };
+    } else {
+        //add search history buttons to page
+        for (var i = 0; i < searchHistoryArray.searchedCity.length; i++) {
+            searchHistory(searchHistoryArray.searchedCity[i]);
+        }
+    }
+
+    return searchHistoryArray;
+}
