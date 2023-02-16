69fee6931cb8b37f71fbd02c08517c788e8f7a7e
VitaliyZhark <vitaliy.zhark@gmail.com>
2023-02-16 14:05:30 +0000

    Update
    Update

--- script.js
+++ script.js
@@ -200,4 +200,43 @@ function getWeather(city) {
                                             .addClass('card-title')
                                             .text(fiveDayArray[i]);
 
-                                            
+                                            // create the icon for current day weather
+                                        var forecastIcon = weatherData.daily[i].weather[0].icon;
+
+                                        var forecastIconEl = $('<img>')
+                                            .attr({
+                                                src: weatherIconUrl + forecastIcon + '.png',
+                                                alt: 'Weather Icon'
+                                            });
+
+                                        // create card text displaying weather details
+                                        var currWeatherDetails = ['Temp: ' + weatherData.current.temp + ' °F', 'Wind: ' + weatherData.current.wind_speed + ' MPH', 'Humidity: ' + weatherData.current.humidity + '%', 'UV Index: ' + weatherData.current.uvi]
+                                        //create temp
+                                        var tempEL = $('<p>')
+                                            .addClass('card-text')
+                                            .text('Temp: ' + weatherData.daily[i].temp.max)
+                                        //create wind
+                                        var windEL = $('<p>')
+                                            .addClass('card-text')
+                                            .text('Wind: ' + weatherData.daily[i].wind_speed + ' MPH')
+                                        // create humidity
+                                        var humidityEL = $('<p>')
+                                            .addClass('card-text')
+                                            .text('Humidity: ' + weatherData.daily[i].humidity + '%')
+
+
+                                        //append cardDivEl to the #five-day container
+                                        fiveDayEl.append(cardDivEl);
+                                        //append cardBodyDivEL to cardDivEl
+                                        cardDivEl.append(cardBodyDivEl);
+                                        //append card title to card body
+                                        cardBodyDivEl.append(cardTitleEl);
+                                        //append icon to card body
+                                        cardBodyDivEl.append(forecastIconEl);
+                                        //append temp details to card body
+                                        cardBodyDivEl.append(tempEL);
+                                        //append wind details to card body
+                                        cardBodyDivEl.append(windEL);
+                                        //append humidity details to card body
+                                        cardBodyDivEl.append(humidityEL);
+                                    }
